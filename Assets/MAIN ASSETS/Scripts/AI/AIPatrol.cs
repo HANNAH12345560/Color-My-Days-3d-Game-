62b8ac56500690020711dac3da67b35d861a0550
Hannah Mae P. Reyes <149644808+HANNAH12345560@users.noreply.github.com>
2025-05-26 20:53:07 +0800

    MainMenu and Lvl7

--- Assets/MAIN ASSETS/Scripts/AI/AIPatrol.cs
+++ Assets/MAIN ASSETS/Scripts/AI/AIPatrol.cs
@@ -2,63 +2,96 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class AIPatrol : MonoBehaviour
 {
     public GameManager gameManager;
     public NavMeshAgent navMeshAgent;
-
     public Transform Player;
+    public TMP_Text txtQuestionUpdate;
+    public TMP_Text txtInteractMsg;
 
-    public bool isAIActive = false;
+    [Header("AI Follow Offset")]
+    public Vector3 followOffset; 
+
+    [Header("Patrol Settings")]
+    public float patrolSpeed = 0.5f;
+    public float followSpeed = 0.5f;
+    public float patrolTime = 5f;
+    public float patrolRadius = 10f;
+
+    private bool isAIActive = false;
+    private bool isPatrolling = true;
+    private float patrolTimer = 0f;
+    private Vector3 patrolTarget;
 
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
         navMeshAgent.autoBraking = false;
+        navMeshAgent.speed = patrolSpeed;
+        SetNewPatrolDestination();
     }
 
     void Update()
     {
-        if (isAIActive)
+        if (isPatrolling)
         {
-            navMeshAgent.SetDestination(Player.transform.position);
+            patrolTimer += Time.deltaTime;
+            if (!navMeshAgent.pathPending && navMeshAgent.remainingDistance < 0.5f)
+            {
+                SetNewPatrolDestination();
+            }
+            if (patrolTimer >= patrolTime)
+            {
+                isPatrolling = false;
+                isAIActive = true;
+                navMeshAgent.speed = followSpeed;
+            }
         }
 
-        else
+        if (isAIActive)
         {
-            navMeshAgent.SetDestination(Player.transform.position);
+            Vector3 targetPosition = Player.transform.position + Player.transform.TransformDirection(followOffset);
+            navMeshAgent.SetDestination(targetPosition);
+        }
+    }
+
+    void SetNewPatrolDestination()
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * patrolRadius;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomDirection, out hit, patrolRadius, NavMesh.AllAreas))
+        {
+            patrolTarget = hit.position;
+            navMeshAgent.SetDestination(patrolTarget);
         }
     }
 
     void OnTriggerEnter(Collider actor)
     {
         if (actor.gameObject.CompareTag("Player"))
-    
-    {
-            isAIActive = true;
-        }
-    }
-
-    void OnTriggerExit(Collider actor)
-    {
-        if (actor.gameObject.CompareTag("Player"))
-    
-    {
-            isAIActive = false;
-        }
-    }
-
-    void OnCollisionEnter(Collider actor)
-    {
-        if (actor.gameObject.CompareTag("Player"))
         {
+            if (txtInteractMsg != null)
+                txtInteractMsg.text = "Hey! What are you doing here?!";
+
+            isAIActive = true;
+            isPatrolling = false;
+            navMeshAgent.speed = followSpeed;
+            Invoke("LoadLevel7", 2f);
         }
     }
 
-    void ReloadScene()
+    void LoadLevel7()
     {
-        //SceneManager.LoadScene("lvl" + gameManager.levelAt);
+        SceneManager.LoadScene("Level_7");
     }
 
+    public void DeactivateAI()
+    {
+        isAIActive = false;
+    }
 }
